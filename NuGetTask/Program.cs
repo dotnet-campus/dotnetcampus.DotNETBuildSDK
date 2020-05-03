b2bcac09217a8801dad6277a454d8f6cfce611bf
lindexi <lindexi_gd@163.com>
2020-05-03 19:37:43 +0800

    荒原

---
+++ NuGetTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using dotnetCampus.DotNETBuild.Context;
+using dotnetCampus.DotNETBuild.Utils;
+
+namespace dotnetCampus.NuGetTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nuGet = new NuGet(AppConfigurator.GetAppConfigurator());
+            nuGet.Restore();
+
+            // todo 支持更多命令
+        }
+    }
+}
