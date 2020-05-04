28e7b04c8e7554c4a2e006cb77a610fa2182e88c
lindexi <lindexi_gd@163.com>
2020-05-04 10:32:04 +0800

    添加构建命令

---
+++ MSBuildTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using dotnetCampus.DotNETBuild.Context;
+using dotnetCampus.DotNETBuild.Utils;
+
+namespace MSBuildTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msBuild = new MsBuild(AppConfigurator.GetAppConfigurator());
+            msBuild.Build();
+        }
+    }
+}
