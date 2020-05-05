e14e01d15f5b162671fc06270949a39600102709
lindexi <lindexi_gd@163.com>
2020-05-05 19:19:01 +0800

    提供默认上传服务器任务

--- DotNETBuild/Context/LogConfiguration.cs
+++ DotNETBuild/Context/LogConfiguration.cs
@@ -15,5 +15,29 @@ namespace dotnetCampus.DotNETBuild.Utils
             set => SetValue(value);
             get => GetString();
         }
+
+        public LogLevel LogLevel
+        {
+            set => SetValue(value.ToString());
+            get
+            {
+                var configurationString = GetString();
+                if (configurationString == null)
+                {
+                    // 默认输出等级
+                    return LogLevel.Info;
+                }
+                else
+                {
+                    if (LogLevel.TryParse(configurationString, out LogLevel logLevel))
+                    {
+                        return logLevel;
+                    }
+
+                    // 默认输出等级
+                    return LogLevel.Info;
+                }
+            }
+        }
     }
 }
