37a93ba061272e23028d9f67a6f15b7b57bd55fe
walterlv <xblero@gmail.com>
2020-07-02 13:43:55 +0800

    添加 MatrixRun 工具

---
+++ MatrixRun/Options.cs
@@ -0,0 +1,13 @@
+﻿using dotnetCampus.Cli;
+
+namespace MatrixRun
+{
+    internal class Options
+    {
+        [Option('m', nameof(Matrix))]
+        public string? Matrix { get; set; }
+
+        [Option('c', nameof(Command))]
+        public string? Command { get; set; }
+    }
+}
