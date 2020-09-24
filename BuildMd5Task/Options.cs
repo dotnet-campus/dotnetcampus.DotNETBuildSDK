00abc69abf15fe736234c988d21f5e41e18b54db
lindexi <lindexi_gd@163.com>
2020-09-24 14:18:26 +0800

    修复命令行
    将大写字符修改为小写字符

--- BuildMd5Task/Options.cs
+++ BuildMd5Task/Options.cs
@@ -10,13 +10,13 @@ namespace dotnetCampus.BuildMd5Task
         /// 传入文件夹时，将会创建每个文件的校验到相同的输出文件
         /// </para>
         /// </summary>
-        [Option('P', nameof(Path))]
+        [Option('p', nameof(Path))]
         public string? Path { get; set; }
 
         /// <summary>
         /// 输出的校验文件，默认将会输出为当前工作路径的 <see cref="DefaultOutputFile"/> 文件
         /// </summary>
-        [Option('O', "Output")]
+        [Option('o', "Output")]
         public string? OutputFile { get; set; }
 
         public const string DefaultOutputFile = "ChecksumMd5.txt";
