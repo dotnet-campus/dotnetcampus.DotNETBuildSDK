2877b721b0b73a60c59cb579d5d7000eb70025f5
lindexi <lindexi_gd@163.com>
2020-09-24 14:37:23 +0800

    修改读取相对路径方法

--- BuildMd5Task/Program.cs
+++ BuildMd5Task/Program.cs
@@ -63,13 +63,46 @@ namespace dotnetCampus.BuildMd5Task
             {
                 fileMd5List.Add(new FileMd5Info()
                 {
-                    File = file.FullName.Remove(0, directory.FullName.Length),
+                    File = MakeRelativePath(directory.FullName, file.FullName),
                     FileSize = file.Length,
                     Md5 = GetMd5Hash(file)
                 });
             }
 
             WriteAsJson(fileMd5List, outputFile);
+        }
+
+        private static string MakeRelativePath(string fromPath, string toPath)
+        {
+            if (string.IsNullOrEmpty(fromPath)) throw new ArgumentNullException(nameof(fromPath));
+            if (string.IsNullOrEmpty(toPath)) throw new ArgumentNullException(nameof(toPath));
+
+            var fromUri = new Uri(fromPath);
+            var toUri = new Uri(toPath);
+
+            if (fromUri.Scheme != toUri.Scheme)
+            {
+                // 不是同一种路径，无法转换成相对路径。
+                return toPath;
+            }
+
+            if (fromUri.Scheme.Equals("file", StringComparison.InvariantCultureIgnoreCase)
+                && !fromPath.EndsWith("/", StringComparison.OrdinalIgnoreCase)
+                && !fromPath.EndsWith("\\", StringComparison.OrdinalIgnoreCase))
+            {
+                // 如果是文件系统，则视来源路径为文件夹。
+                fromUri = new Uri(fromPath + Path.DirectorySeparatorChar);
+            }
+
+            var relativeUri = fromUri.MakeRelativeUri(toUri);
+            var relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+            if (toUri.Scheme.Equals("file", StringComparison.InvariantCultureIgnoreCase))
+            {
+                relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+            }
+
+            return relativePath;
         }
 
         private static void WriteAsJson(List<FileMd5Info> fileMd5List, string outputFile)
