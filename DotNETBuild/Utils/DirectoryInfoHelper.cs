9d274a77e09c7a557cf638e4b6d6dd3fc58ab740
lindexi <lindexi_gd@163.com>
2020-09-25 10:35:46 +0800

    加上文件夹辅助类

---
+++ DotNETBuild/Utils/DirectoryInfoHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace dotnetCampus.DotNETBuild.Utils
+{
+    /// <summary>
+    /// 文件夹辅助类
+    /// </summary>
+    public static class DirectoryInfoHelper
+    {
+        /// <summary>
+        /// 通过正则匹配文件名获取文件
+        /// </summary>
+        public static IEnumerable<FileInfo> GetFilesWithRegexPattern(this DirectoryInfo directory, Regex regex)
+        {
+            foreach (var file in directory.EnumerateFiles("*", SearchOption.AllDirectories))
+            {
+                if (regex.IsMatch(file.Name))
+                {
+                    yield return file;
+                }
+            }
+        }
+    }
+}
