b228fe337464f30aeceb85b2a983338cb275d23a
lindexi <lindexi_gd@163.com>
2021-01-18 10:37:00 +0800

    加入程序集信息读取

---
+++ DotNETBuild/Utils/AssemblyHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using dotnetCampus.Configurations;
+using dotnetCampus.DotNETBuild.Context;
+
+namespace dotnetCampus.DotNETBuild.Utils
+{
+    public static class AssemblyHelper
+    {
+        /// <summary>
+        /// 填充程序集信息，包括版本号
+        /// </summary>
+        /// <returns></returns>
+        public static IAppConfigurator FillAssemblyInfo(this IAppConfigurator appConfigurator, Assembly assembly)
+        {
+            var compileConfiguration = appConfigurator.Of<CompileConfiguration>();
+
+            var assemblyInformationalVersionAttribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+            if (string.IsNullOrEmpty(compileConfiguration.AppVersion))
+            {
+                compileConfiguration.AppVersion = assemblyInformationalVersionAttribute!.InformationalVersion;
+            }
+
+            if (string.IsNullOrEmpty(compileConfiguration.AssemblyVersion))
+            {
+                compileConfiguration.AssemblyVersion = assemblyInformationalVersionAttribute!.InformationalVersion;
+            }
+
+            return appConfigurator;
+        }
+    }
+}
