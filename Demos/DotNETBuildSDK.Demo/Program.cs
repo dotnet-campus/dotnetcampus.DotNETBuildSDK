da7f735b9018d5bc0024d6a14c3a2bc9f50246bf
lindexi <lindexi_gd@163.com>
2021-08-15 11:08:01 +0800

    应用启动，输出第一条日志，先输出框架日志

--- Demos/DotNETBuildSDK.Demo/Program.cs
+++ Demos/DotNETBuildSDK.Demo/Program.cs
@@ -1,27 +1,45 @@
-﻿using System;
+﻿using dotnetCampus.DotNETBuild.Utils;
+using System;
 using System.Threading.Tasks;
 
 namespace DotNETBuildSDK.Demo
 {
     class Program
     {
-        //应用只是启动，没有设置日志等级，也没有输出任何日志，此时日志将会在 Main 结束输出
         static void Main(string[] args)
         {
+            //// 应用只是启动，没有设置日志等级，也没有输出任何日志，此时日志将会在 Main 结束输出
+            //dotnetCampus.DotNETBuild.CommandLineDragonFruit
+            //    .CommandLine
+            //    .ExecuteAssemblyAsync(entryAssembly: typeof(Program).Assembly,
+            //  args: args,
+            //  "DotNETBuildSDK.Demo.FakeProgram1").Wait();
+
+            // 应用启动，输出第一条日志，将会在输出第一条日志之前，先输出框架日志
             dotnetCampus.DotNETBuild.CommandLineDragonFruit
                 .CommandLine
                 .ExecuteAssemblyAsync(entryAssembly: typeof(Program).Assembly,
               args: args,
-              "DotNETBuildSDK.Demo.FakeProgram1").Wait();
+              "DotNETBuildSDK.Demo.FakeProgram2").Wait();
         }
     }
 
     class FakeProgram1
     {
-        //应用只是启动，没有设置日志等级，也没有输出任何日志，此时日志将会在 Main 结束输出
+        // 应用只是启动，没有设置日志等级，也没有输出任何日志，此时日志将会在 Main 结束输出
         static void Main(string[] args)
         {
 
+        }
+    }
+
+    class FakeProgram2
+    {
+        // 应用启动，输出第一条日志，将会在输出第一条日志之前，先输出框架日志
+        static void Main(string[] args)
+        {
+            // 预期在输出 Foo 之前，先输出框架的日志
+            Log.Info("Foo");
         }
     }
 }
