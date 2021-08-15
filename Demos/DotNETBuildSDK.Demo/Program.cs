a29e6ad72a1451a8f4e954bb89b8163dcf365385
lindexi <lindexi_gd@163.com>
2021-08-15 10:35:51 +0800

    加上测试应用

---
+++ Demos/DotNETBuildSDK.Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DotNETBuildSDK.Demo
+{
+    class Program
+    {
+        //应用只是启动，没有设置日志等级，也没有输出任何日志，此时日志将会在 Main 结束输出
+        static void Main(string[] args)
+        {
+            dotnetCampus.DotNETBuild.CommandLineDragonFruit
+                .CommandLine
+                .ExecuteAssemblyAsync(entryAssembly: typeof(Program).Assembly,
+              args: args,
+              "DotNETBuildSDK.Demo.FakeProgram1").Wait();
+        }
+    }
+
+    class FakeProgram1
+    {
+        //应用只是启动，没有设置日志等级，也没有输出任何日志，此时日志将会在 Main 结束输出
+        static void Main()
+        {
+
+        }
+    }
+}
