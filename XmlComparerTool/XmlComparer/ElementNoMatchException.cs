35af2c1d21946513f892a28880998795ca39293e
lindexi <lindexi_gd@163.com>
2021-09-13 19:27:05 +0800

    优化定义

---
+++ XmlComparerTool/XmlComparer/ElementNoMatchException.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace dotnetCampus.Comparison
+{
+    /// <summary>
+    /// 元素不匹配异常
+    /// </summary>
+    public class ElementNoMatchException : ArgumentException
+    {
+        /// <summary>
+        /// 创建元素不匹配异常
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="element1"></param>
+        /// <param name="element2"></param>
+        public ElementNoMatchException(string message, XElement element1, XElement? element2) : base(message)
+        {
+            Element1 = element1;
+            Element2 = element2;
+
+            if (element1 is IXmlLineInfo xmlLineInfo)
+            {
+                LineNumber = xmlLineInfo.LineNumber;
+            }
+        }
+
+        /// <summary>
+        /// 第一个 XML 元素
+        /// </summary>
+        public XElement Element1 { get; }
+
+        /// <summary>
+        /// 第二个 XML 元素
+        /// </summary>
+        public XElement? Element2 { get; }
+
+        /// <summary>
+        /// 行数
+        /// </summary>
+        public int LineNumber { get; }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return $"ElementName:{Element1.Name};\r\nLineNumber:{LineNumber};\r\n{base.ToString()}";
+        }
+    }
+}
--- XmlComparerTool/XmlComparer/XmlComparer.cs
+++ XmlComparerTool/XmlComparer/XmlComparer.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Xml;
 using System.Xml.Linq;
 
 namespace dotnetCampus.Comparison
@@ -120,50 +119,6 @@ namespace dotnetCampus.Comparison
             {
                 throw new ElementNoMatchException(message, xElement1, xElement2);
             }
-        }
-    }
-
-    /// <summary>
-    /// 元素不匹配异常
-    /// </summary>
-    public class ElementNoMatchException : ArgumentException
-    {
-        /// <summary>
-        /// 创建元素不匹配异常
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="element1"></param>
-        /// <param name="element2"></param>
-        public ElementNoMatchException(string message, XElement element1, XElement? element2) : base(message)
-        {
-            Element1 = element1;
-            Element2 = element2;
-
-            if (element1 is IXmlLineInfo xmlLineInfo)
-            {
-                LineNumber = xmlLineInfo.LineNumber;
-            }
-        }
-
-        /// <summary>
-        /// 第一个 XML 元素
-        /// </summary>
-        public XElement Element1 { get; }
-
-        /// <summary>
-        /// 第二个 XML 元素
-        /// </summary>
-        public XElement? Element2 { get; }
-
-        /// <summary>
-        /// 行数
-        /// </summary>
-        public int LineNumber { get; }
-
-        /// <inheritdoc />
-        public override string ToString()
-        {
-            return $"ElementName:{Element1.Name};\r\nLineNumber:{LineNumber};\r\n{base.ToString()}";
         }
     }
 }
