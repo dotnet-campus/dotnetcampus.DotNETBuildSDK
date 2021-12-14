59e59a5edcbfb1d72f41877eca3415a51440d47c
lindexi <lindexi_gd@163.com>
2021-12-14 10:12:58 +0800

    提供使用正则进行替换内容的功能

--- RegexReplaceTask/Program.cs
+++ RegexReplaceTask/Program.cs
@@ -1,4 +1,7 @@
-﻿using dotnetCampus.Cli;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+using dotnetCampus.Cli;
 
 namespace dotnetCampus.RegexReplaceTask
 {
@@ -7,19 +10,48 @@ namespace dotnetCampus.RegexReplaceTask
         public static void Main(string[] args)
         {
             var options = dotnetCampus.Cli.CommandLine.Parse(args).As<Options>();
+            if (string.IsNullOrEmpty(options.FilePath) || !File.Exists(options.FilePath))
+            {
+                throw new FileNotFoundException($"Can not find {options.FilePath}.FullPath={Path.GetFullPath(options.FilePath)}");
+            }
 
+            var text = File.ReadAllText(options.FilePath);
+            var matchOutputText = Regex.Match(text, options.ReplaceRegex);
+
+            if (!matchOutputText.Success)
+            {
+                throw new ArgumentException(
+                    $"Can not match regex={options.ReplaceRegex} in OutputFile={options.FilePath} \r\n The file content is\r\n {text}");
+            }
+
+            var replaceText = matchOutputText.Groups[0].Value.Replace(matchOutputText.Groups[1].Value, options.Value);
+            Console.WriteLine($"Replace {matchOutputText.Groups[0].Value} to {replaceText}");
+            text = text.Replace(matchOutputText.Groups[0].Value, replaceText);
+            File.WriteAllText(options.FilePath, text);
         }
 
         class Options
         {
+            /// <summary>
+            /// 将替换为的内容
+            /// </summary>
             [Option('v', "Value")]
-            public string Value { set; get; }
+            [NotNull]
+            public string? Value { set; get; }
 
+            /// <summary>
+            /// 用于替换的正则表达式，其中 match.Groups[1].Value 将被替换为 <see cref="Value"/> 的值
+            /// </summary>
             [Option('r', "Regex")]
-            public string ReplaceRegex { set; get; }
+            [NotNull]
+            public string? ReplaceRegex { set; get; }
 
+            /// <summary>
+            /// 替换内容的文件，要求文件一定存在
+            /// </summary>
             [Option('f', "File")]
-            public string FilePath { set; get; }
+            [NotNull]
+            public string? FilePath { set; get; }
         }
     }
 }
