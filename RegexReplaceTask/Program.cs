75dd32202b54cde1d18ae81a8022d882b79dfdab
lindexi <lindexi_gd@163.com>
2021-12-14 08:53:21 +0800

    加上使用正则替换内容的代码

---
+++ RegexReplaceTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using dotnetCampus.Cli;
+
+namespace dotnetCampus.RegexReplaceTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var options = dotnetCampus.Cli.CommandLine.Parse(args).As<Options>();
+
+        }
+
+        class Options
+        {
+            [Option('v', "Value")]
+            public string Value { set; get; }
+
+            [Option('r', "Regex")]
+            public string ReplaceRegex { set; get; }
+
+            [Option('f', "File")]
+            public string FilePath { set; get; }
+        }
+    }
+}
