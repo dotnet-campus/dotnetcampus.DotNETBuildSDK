fffb72dc95d56cb84f1c88f2184044d18f02d836
lindexi <lindexi_gd@163.com>
2022-01-06 17:12:39 +0800

    加上注释

--- DotNETBuild/Utils/BuildMd5Tools/DirectoryCheckingResult.cs
+++ DotNETBuild/Utils/BuildMd5Tools/DirectoryCheckingResult.cs
@@ -2,8 +2,14 @@
 
 namespace dotnetCampus.DotNETBuild.Utils.BuildMd5Tools
 {
+    /// <summary>
+    ///     文件夹校验结果
+    /// </summary>
     public class DirectoryCheckingResult
     {
+        /// <summary>
+        ///     是否所有的文件都符合预期，全都匹配上校验值
+        /// </summary>
         public bool AreAllMatched { set; get; }
 
         /// <summary>
