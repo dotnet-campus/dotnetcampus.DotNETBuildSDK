c3440dfea94fc206c6379f321d3e7e9ec653c000
lindexi <lindexi_gd@163.com>
2022-01-28 14:31:17 +0800

    加上命令行执行进程辅助方法

---
+++ DotNETBuild/Utils/ProcessRunner_/OutputType.cs
@@ -0,0 +1,17 @@
+﻿namespace dotnetCampus.DotNETBuild.Utils;
+
+/// <summary>
+/// 输出类型
+/// </summary>
+public enum OutputType
+{
+    /// <summary>
+    /// 标准输出，对应 StandardOutput 内容
+    /// </summary>
+    StandardOutput,
+
+    /// <summary>
+    /// 输出错误信息，对应 StandardError 内容
+    /// </summary>
+    StandardError,
+}
---
+++ DotNETBuild/Utils/ProcessRunner_/ProcessOutputInfo.cs
@@ -0,0 +1,38 @@
+﻿#nullable enable
+using System;
+
+namespace dotnetCampus.DotNETBuild.Utils;
+
+/// <summary>
+/// 进程输出消息
+/// </summary>
+public readonly struct ProcessOutputInfo
+{
+    /// <summary>
+    /// 创建进程输出消息
+    /// </summary>
+    /// <param name="outputType"></param>
+    /// <param name="message"></param>
+    public ProcessOutputInfo(OutputType outputType, string? message)
+    {
+        OutputType = outputType;
+        Message = message;
+
+        OutputTime = DateTime.Now;
+    }
+
+    /// <summary>
+    /// 输出类型
+    /// </summary>
+    public OutputType OutputType { get; }
+
+    /// <summary>
+    /// 消息
+    /// </summary>
+    public string? Message { get; }
+
+    /// <summary>
+    /// 何时输出
+    /// </summary>
+    public DateTimeOffset OutputTime { get; }
+}
