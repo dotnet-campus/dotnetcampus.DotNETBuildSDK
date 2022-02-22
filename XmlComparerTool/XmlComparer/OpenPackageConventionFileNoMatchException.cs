47ffc3bcdbb56d656c8d6c5a97af8d1d873ae103
lindexi <lindexi_gd@163.com>
2022-02-22 11:37:53 +0800

    加上对比使用的 XML 文件路径，方便调试

--- XmlComparerTool/XmlComparer/OpenPackageConventionFileNoMatchException.cs
+++ XmlComparerTool/XmlComparer/OpenPackageConventionFileNoMatchException.cs
@@ -15,13 +15,17 @@ namespace dotnetCampus.Comparison
         /// <param name="xmlFileName"></param>
         /// <param name="opcFile1"></param>
         /// <param name="opcFile2"></param>
+        /// <param name="xmlFile1"></param>
+        /// <param name="xmlFile2"></param>
         public OpenPackageConventionFileNoMatchException(ElementNotMatchException exception, string xmlFileName,
-            FileInfo opcFile1, FileInfo opcFile2) : base(ToString(xmlFileName, exception, opcFile1, opcFile2), exception)
+            FileInfo opcFile1, FileInfo opcFile2, FileInfo xmlFile1, FileInfo xmlFile2) : base(ToString(xmlFileName, exception, opcFile1, opcFile2), exception)
         {
             Exception = exception;
             XmlFileName = xmlFileName;
             OpcFile1 = opcFile1;
             OpcFile2 = opcFile2;
+            XmlFile1 = xmlFile1;
+            XmlFile2 = xmlFile2;
         }
 
         /// <summary>
@@ -35,14 +39,29 @@ namespace dotnetCampus.Comparison
         public string XmlFileName { get; }
 
         /// <summary>
-        /// 对比的文件
+        /// 对比的 OPC 文件
         /// </summary>
         public FileInfo OpcFile1 { get; }
 
         /// <summary>
-        /// 对比的文件
+        /// 对比的 OPC 文件
         /// </summary>
         public FileInfo OpcFile2 { get; }
+
+        /// <summary>
+        /// 对比的 XML 文件
+        /// </summary>
+        public FileInfo XmlFile1 { get; }
+
+        /// <summary>
+        /// 对比的 XML 文件
+        /// </summary>
+        public FileInfo XmlFile2 { get; }
+
+        /// <summary>
+        /// 不匹配元素所在文档的行数
+        /// </summary>
+        public int LineNumber => Exception.LineNumber;
 
         /// <inheritdoc />
         public override string ToString()
