0561dbb2fbc683f255292600ae40e0e0bcc92733
lindexi <lindexi_gd@163.com>
2023-02-04 15:53:00 +0800

    升级库 减少重复代码

--- SendEmailTask/Program.cs
+++ SendEmailTask/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using dotnetCampus.DotNETBuild.Context;
+using dotnetCampus.DotNETBuild.Utils;
 
 namespace dotnetCampus.SendEmailTask
 {
