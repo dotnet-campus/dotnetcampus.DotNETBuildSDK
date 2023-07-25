7ce08b321732b85494bbd21f75d7a18f278b7177
baozhangchi <baozhangchi@live.com>
2023-07-25 18:02:41 +0800

    执行前删除旧文件

--- PublishFolderCleaner/PublishFolderCleaner/Program.cs
+++ PublishFolderCleaner/PublishFolderCleaner/Program.cs
@@ -1,29 +1,36 @@
-﻿using System;
+﻿#region
+
 using System.IO;
 using dotnetCampus.Cli;
 
-namespace PublishFolderCleaner
+#endregion
+
+namespace PublishFolderCleaner;
+
+internal class Program
 {
-    class Program
+    #region Methods
+
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var options = CommandLine.Parse(args).As<Options>();
+        var options = CommandLine.Parse(args).As<Options>();
 
-            const string libFolderName = "lib";
-            var publishFolder = options.PublishFolder.Trim();
-            var libFolder = Path.GetFullPath(Path.Combine(publishFolder, libFolderName));
-            var tempFolder = Path.GetFullPath(Path.Combine(publishFolder, @"..", Path.GetRandomFileName()));
-            Directory.Move(publishFolder, tempFolder);
-            Directory.CreateDirectory(publishFolder);
-            Directory.Move(tempFolder, libFolder);
+        const string libFolderName = "lib";
+        var publishFolder = options.PublishFolder.Trim();
+        var libFolder = Path.GetFullPath(Path.Combine(publishFolder, libFolderName));
+        var tempFolder = Path.GetFullPath(Path.Combine(publishFolder, @"..", Path.GetRandomFileName()));
+        if (Directory.Exists(libFolder)) Directory.Delete(libFolder, true);
+        Directory.Move(publishFolder, tempFolder);
+        Directory.CreateDirectory(publishFolder);
+        Directory.Move(tempFolder, libFolder);
 
-            var appHostFilePath = Path.Combine(libFolder, options.ApplicationName + ".exe");
-            var newAppHostFilePath = Path.Combine(publishFolder, options.ApplicationName + ".exe");
+        var appHostFilePath = Path.Combine(libFolder, options.ApplicationName + ".exe");
+        var newAppHostFilePath = Path.Combine(publishFolder, options.ApplicationName + ".exe");
 
-            File.Move(appHostFilePath, newAppHostFilePath);
+        File.Move(appHostFilePath, newAppHostFilePath);
 
-            AppHostPatcher.Patch(newAppHostFilePath, Path.Combine("lib", options.ApplicationName + ".dll"));
-        }
+        AppHostPatcher.Patch(newAppHostFilePath, Path.Combine("lib", options.ApplicationName + ".dll"));
     }
+
+    #endregion
 }
