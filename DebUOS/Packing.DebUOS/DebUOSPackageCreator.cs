0a14fb8b07f6f81cb4597821d28e0c1709c90963
lindexi <lindexi_gd@163.com>
2023-12-27 14:25:20 +0800

    开始获取项目信息尝试进入调试

--- DebUOS/Packing.DebUOS/DebUOSPackageCreator.cs
+++ DebUOS/Packing.DebUOS/DebUOSPackageCreator.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using dotnetCampus.MSBuildUtils;
 
 namespace Packing.DebUOS;
 
 public class DebUOSPackageCreator
 {
+  
 }
