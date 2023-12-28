7da0096edcb826539ebea07223f6eb5ae284e6ca
lindexi <lindexi_gd@163.com>
2023-12-28 15:43:26 +0800

    添加快速调试代码

--- DebUOS/Packing.DebUOS.Tool/Program.cs
+++ DebUOS/Packing.DebUOS.Tool/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using dotnetCampus.Configurations;
+using dotnetCampus.Configurations.Core;
+
+using Packing.DebUOS;
+
+//var argsFilePath = args[0];
+//var fileConfigurationRepo = ConfigurationFactory.FromFile(argsFilePath,RepoSyncingBehavior.Static);
+//var appConfigurator = fileConfigurationRepo.CreateAppConfigurator();
+
+var debUosPackageCreator = new DebUOSPackageCreator();
+debUosPackageCreator.Execute();
+
+Console.Read();
