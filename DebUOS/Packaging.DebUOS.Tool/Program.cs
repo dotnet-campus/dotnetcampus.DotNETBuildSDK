54c9960fb4bea879d7472cfeedc1bce6a6d883ca
lindexi <lindexi_gd@163.com>
2024-01-03 14:54:03 +0800

    尝试加上标准过滤，但是失败

--- DebUOS/Packaging.DebUOS.Tool/Program.cs
+++ DebUOS/Packaging.DebUOS.Tool/Program.cs
@@ -2,6 +2,7 @@
 
 using System.Text;
 using dotnetCampus.Cli;
+using dotnetCampus.Cli.Standard;
 using dotnetCampus.Configurations;
 using dotnetCampus.Configurations.Core;
 using Microsoft.Extensions.Logging;
@@ -10,54 +11,57 @@ using Packaging.DebUOS;
 using Packaging.DebUOS.Contexts.Configurations;
 using Packaging.DebUOS.Tool;
 
-var options = CommandLine.Parse(args).As<Options>();
-
-var loggerFactory = LoggerFactory.Create(builder =>
+CommandLine.Parse(args).AddStandardHandlers().AddHandler((Options options) =>
 {
-    builder.AddSimpleConsole(simpleConsoleFormatterOptions =>
+    var loggerFactory = LoggerFactory.Create(builder =>
     {
-        simpleConsoleFormatterOptions.ColorBehavior = LoggerColorBehavior.Disabled;
-        simpleConsoleFormatterOptions.SingleLine = true;
+        builder.AddSimpleConsole(simpleConsoleFormatterOptions =>
+        {
+            simpleConsoleFormatterOptions.ColorBehavior = LoggerColorBehavior.Disabled;
+            simpleConsoleFormatterOptions.SingleLine = true;
+        });
     });
-});
 
-var logger = loggerFactory.CreateLogger("");
+    var logger = loggerFactory.CreateLogger("");
 
-if (!string.IsNullOrEmpty(options.BuildPath))
-{
-    var packingFolder = new DirectoryInfo(options.BuildPath);
-    var outputPath = options.OutputPath ?? Path.Join(packingFolder.FullName,$"{packingFolder.Name}.deb");
-    var outputDebFile = new FileInfo(outputPath);
-
-    var debUosPackageCreator = new DebUOSPackageCreator(logger);
-    //var packingFolder = new DirectoryInfo(@"C:\lindexi\Work\");
-    //var outputDebFile = new FileInfo(@"C:\lindexi\Work\Downloader.deb");
-    debUosPackageCreator.PackageDeb(packingFolder, outputDebFile);
-}
-else if (!string.IsNullOrEmpty(options.PackageArgumentFilePath))
-{
-    logger.LogInformation($"开始根据配置创建 UOS 的 deb 包。配置文件：{options.PackageArgumentFilePath}");
-    if (!File.Exists(options.PackageArgumentFilePath))
+    if (!string.IsNullOrEmpty(options.BuildPath))
     {
-        logger.LogError($"配置文件 '{options.PackageArgumentFilePath}' 不创建");
-        return;
+        var packingFolder = new DirectoryInfo(options.BuildPath);
+        var outputPath = options.OutputPath ?? Path.Join(packingFolder.FullName, $"{packingFolder.Name}.deb");
+        var outputDebFile = new FileInfo(outputPath);
+
+        var debUosPackageCreator = new DebUOSPackageCreator(logger);
+        //var packingFolder = new DirectoryInfo(@"C:\lindexi\Work\");
+        //var outputDebFile = new FileInfo(@"C:\lindexi\Work\Downloader.deb");
+        debUosPackageCreator.PackageDeb(packingFolder, outputDebFile);
     }
+    else if (!string.IsNullOrEmpty(options.PackageArgumentFilePath))
+    {
+        logger.LogInformation($"开始根据配置创建 UOS 的 deb 包。配置文件：{options.PackageArgumentFilePath}");
+        if (!File.Exists(options.PackageArgumentFilePath))
+        {
+            logger.LogError($"配置文件 '{options.PackageArgumentFilePath}' 不创建");
+            return;
+        }
 
-    var fileConfigurationRepo = ConfigurationFactory.FromFile(options.PackageArgumentFilePath, RepoSyncingBehavior.Static);
-    var appConfigurator = fileConfigurationRepo.CreateAppConfigurator();
-    var configuration = appConfigurator.Of<DebUOSConfiguration>();
+        var fileConfigurationRepo = ConfigurationFactory.FromFile(options.PackageArgumentFilePath, RepoSyncingBehavior.Static);
+        var appConfigurator = fileConfigurationRepo.CreateAppConfigurator();
+        var configuration = appConfigurator.Of<DebUOSConfiguration>();
 
-    var fileStructCreator = new DebUOSPackageFileStructCreator(logger);
-    fileStructCreator.CreatePackagingFolder(configuration);
+        var fileStructCreator = new DebUOSPackageFileStructCreator(logger);
+        fileStructCreator.CreatePackagingFolder(configuration);
 
-    var packingFolder = new DirectoryInfo(configuration.PackingFolder!);
-    var outputDebFile = new FileInfo(configuration.DebUOSOutputFilePath!);
-    var workingFolder = new DirectoryInfo(configuration.WorkingFolder!);
+        var packingFolder = new DirectoryInfo(configuration.PackingFolder!);
+        var outputDebFile = new FileInfo(configuration.DebUOSOutputFilePath!);
+        var workingFolder = new DirectoryInfo(configuration.WorkingFolder!);
 
-    var debUosPackageCreator = new DebUOSPackageCreator(logger);
-    debUosPackageCreator.PackageDeb(packingFolder, outputDebFile,workingFolder);
-}
-else
-{
-    // Show Help
-}
+        var debUosPackageCreator = new DebUOSPackageCreator(logger);
+        debUosPackageCreator.PackageDeb(packingFolder, outputDebFile, workingFolder);
+    }
+    else
+    {
+        // Show Help
+    }
+}).Run();
+
+
