063d75489f645b885c1a69261b1227d6c9fb341f
lindexi <lindexi_gd@163.com>
2024-04-26 19:57:36 +0800

    尝试修复程序集信息加上了 git 的 commit 号导致打包过于诡异

--- DotNETBuild/Utils/AssemblyHelper.cs
+++ DotNETBuild/Utils/AssemblyHelper.cs
@@ -17,6 +17,25 @@ namespace dotnetCampus.DotNETBuild.Utils
         {
             var compileConfiguration = appConfigurator.Of<CompileConfiguration>();
 
+            // 优先从 AssemblyVersionAttribute 取出，解决 [dotnet 8 破坏性改动 在 AssemblyInformationalVersionAttribute 添加上 git 的 commit 号](https://blog.lindexi.com/post/dotnet-8-%E7%A0%B4%E5%9D%8F%E6%80%A7%E6%94%B9%E5%8A%A8-%E5%9C%A8-AssemblyInformationalVersionAttribute-%E6%B7%BB%E5%8A%A0%E4%B8%8A-git-%E7%9A%84-commit-%E5%8F%B7.html )
+            // 
+            if (string.IsNullOrEmpty(compileConfiguration.AppVersion))
+            {
+                var assemblyVersionAttribute = assembly.GetCustomAttribute<AssemblyVersionAttribute>();
+                if (assemblyVersionAttribute != null)
+                {
+                    compileConfiguration.AppVersion = assemblyVersionAttribute.Version;
+                }
+                else
+                {
+                    var assemblyFileVersionAttribute = assembly.GetCustomAttribute<AssemblyFileVersionAttribute>();
+                    if (assemblyFileVersionAttribute != null)
+                    {
+                        compileConfiguration.AppVersion = assemblyFileVersionAttribute.Version;
+                    }
+                }
+            }
+
             var assemblyInformationalVersionAttribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
             if (string.IsNullOrEmpty(compileConfiguration.AppVersion))
             {
