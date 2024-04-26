d6efbeddffd3cb1c72a1acbfe413c2f9b85c549e
lindexi <lindexi_gd@163.com>
2024-04-26 15:27:58 +0800

    加上压缩的辅助方法

---
+++ DotNETBuild/Utils/ZipTool_/ZipArchiveExtension.cs
@@ -0,0 +1,48 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.IO;
+
+namespace dotnetCampus.DotNETBuild.Utils
+{
+    public static class ZipArchiveExtension
+    {
+        /// <summary>
+        /// 追加文件夹到压缩文件里面
+        /// </summary>
+        /// <param name="archive"></param>
+        /// <param name="sourceDirectoryName"></param>
+        /// <param name="zipRelativePath">在压缩包里面的相对路径</param>
+        /// <param name="compressionLevel"></param>
+        /// <param name="fileCanAddedPredicate"></param>
+        public static void AppendDirectoryToZipArchive(this ZipArchive archive, string sourceDirectoryName, string zipRelativePath, CompressionLevel compressionLevel = CompressionLevel.Fastest, Predicate<string>? fileCanAddedPredicate = null)
+        {
+            var folders = new Stack<string>();
+
+            folders.Push(sourceDirectoryName);
+
+            while (folders.Count > 0)
+            {
+                var currentFolder = folders.Pop();
+
+                foreach (var item in Directory.EnumerateFiles(currentFolder))
+                {
+                    if (fileCanAddedPredicate != null && !fileCanAddedPredicate(item))
+                    {
+                        continue;
+                    }
+
+                    archive.CreateEntryFromFile(item, Path.Join(zipRelativePath, Path.GetRelativePath(sourceDirectoryName, item)), compressionLevel);
+                }
+
+                foreach (var item in Directory.EnumerateDirectories(currentFolder))
+                {
+                    folders.Push(item);
+                }
+            }
+        }
+    }
+}
+
