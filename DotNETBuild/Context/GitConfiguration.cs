97910fa30db747cc56e1b39e127f5d9b5690d334
lindexi <lindexi_gd@163.com>
2024-11-13 16:03:15 +0800

    加上更多注释

--- DotNETBuild/Context/GitConfiguration.cs
+++ DotNETBuild/Context/GitConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using dotnetCampus.Configurations;
+using dotnetCampus.DotNETBuild.Utils;
 
 namespace dotnetCampus.DotNETBuild.Context
 {
@@ -13,8 +14,9 @@ namespace dotnetCampus.DotNETBuild.Context
         }
 
         /// <summary>
-        /// 当前的 commit 是这个分支的第几次
+        /// 当前的 commit 是这个分支的第几次。手动调用 <see cref="GitHelper.FillGitInfo"/> 方法将被填充
         /// </summary>
+        /// <remarks>将使用 git rev-list --count HEAD 命令获取</remarks>
         public int? GitCount
         {
             set => SetValue(value);
@@ -22,7 +24,7 @@ namespace dotnetCampus.DotNETBuild.Context
         }
 
         /// <summary>
-        /// 当前的 commit 号
+        /// 当前的 commit 号。手动调用 <see cref="GitHelper.FillGitInfo"/> 方法将被填充
         /// </summary>
         public string CurrentCommit
         {
