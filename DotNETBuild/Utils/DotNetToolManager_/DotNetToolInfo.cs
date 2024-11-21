7bf10a05fc714929aa4a26c0e1367637b6b6ae28
lindexi <lindexi_gd@163.com>
2024-11-21 10:31:07 +0800

    添加更多辅助工具

---
+++ DotNETBuild/Utils/DotNetToolManager_/DotNetToolInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace dotnetCampus.DotNETBuild.Utils;
+
+public class DotNetToolInfo
+{
+    public DotNetToolInfo(string packageId, string version, string commands)
+    {
+        PackageId = packageId;
+        Version = version;
+        Commands = commands;
+    }
+
+    public string PackageId { get; }
+    public string Version { get; }
+    public string Commands { get; }
+}
